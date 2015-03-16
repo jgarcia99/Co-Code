23a5dc13ff8b81f43276ee787086461d9b1d3b93
Josuel C Garcia <garciajosuel@yahoo.com>
2015-03-15 21:26:34 -0400

    need lodash for angular map

--- src/app/maps/maps.controller.js
+++ src/app/maps/maps.controller.js
@@ -1,22 +1,21 @@
 'use strict'
 
 angular.module('coCode')
+  .controller('MapsCtrl', function ($scope) {
 
-.controller('MapsCtrl', function($scope) {
-
-    $scope.maps = {
+    $scope.map = {
         center: {
-            latitude: 28,
-            logitude: -81
+            latitude: 42 ,
+            logitude: -90
         },
-        zoom: 8
+        zoom: 5
     };
 
-    $scope.markerList = [
-      {latitude: 28.593178, logitude: -81.361418,},
-      {latitude: 28.568422, logitude: -81.346529,},
-      {latitude:  28.571854, logitude: -81.389863,},
-      {latitude: 28.549242, logitude: -81.352306,},
-      {latitude: 28.555688, logitude: -81.207649,}
-    ]
+    // $scope.markerList = [
+    //   {latitude: 28.593178, logitude: -81.361418,},
+    //   {latitude: 28.568422, logitude: -81.346529,},
+    //   {latitude:  28.571854, logitude: -81.389863,},
+    //   {latitude: 28.549242, logitude: -81.352306,},
+    //   {latitude: 28.555688, logitude: -81.207649,}
+    // ]
 });
