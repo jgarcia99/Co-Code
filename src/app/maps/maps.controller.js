57c92f6ad190d466f5ab4e6b92bf53bb132094db
Josuel C Garcia <garciajosuel@yahoo.com>
2015-03-17 00:32:14 -0400

    so self needed to be in single and double quotes

--- src/app/maps/maps.controller.js
+++ src/app/maps/maps.controller.js
@@ -4,45 +4,37 @@ angular.module('coCode')
         $scope.map = {
 
             center: {
-                latitude: 28,
-                longitude: -81
+                latitude: 28.593178,
+                longitude: -81.361418
             },
-            zoom: 7
+            zoom: 11
         };
 
-        $scope.markerList = [
-        {
-            // id: 1,
-            // coords: {
-                latitude: 28.593178,
-                longitude: -81.361418,
-            // }
+        $scope.coffeeMarkers = [{
+            id: 1,
+            latitude: 28.593178,
+            longitude: -81.361418,
+
         }, {
-            // id: 2,
-            // coords: {
-                latitude: 28.568422,
-                longitude: -81.346529
-            // }
+            id: 2,
+            latitude: 28.568422,
+            longitude: -81.346529
+
         }, {
-            // id: 3,
-            // coords: {
-                latitude: 28.571854,
-                longitude: -81.389863
-            // }
+            id: 3,
+            latitude: 28.571854,
+            longitude: -81.389863
+
         }, {
-            // id: 4,
-            // coords: {
-                latitude: 28.549242,
-                longitude: -81.352306
-            // }
+            id: 4,
+            latitude: 28.549242,
+            longitude: -81.352306
         }, {
-            // id: 5,
-            // coords: {
-                latitude: 28.555688,
-                longitude: -81.207649
-            // }
+
+            id: 5,
+            latitude: 28.555688,
+            longitude: -81.207649
+
         }];
 
     });
-
-
