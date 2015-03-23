e217c29e5ad1caab8086866228fc17d66dce02c7
Josuel C Garcia <garciajosuel@yahoo.com>
2015-03-23 04:31:43 -0400

    dist/ not ignored

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 'use strict';
 
 var gulp = require('gulp');
+var ghPages = require('gulp-gh-pages');
+
+gulp.task('deploy', function() {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages());
+});
 
 gulp.paths = {
   src: 'src',
